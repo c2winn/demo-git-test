e55319359601a3e9ded0e71e23516125669006ee
linhtetaung-gic <linhtetaung.miit@gmail.com>
2025-03-24 13:20:57 +0630

    LHA

--- src/main/java/com/demo/demo/Controller/HomeController.java
+++ src/main/java/com/demo/demo/Controller/HomeController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class HomeController {
-    @RequestMapping(path="/", method=RequestMethod.GET)
+    @RequestMapping(path = "/", method = RequestMethod.GET)
     public String requestMethodName(@RequestParam String param) {
+        // I am Lin Htet Aung
         return new String();
     }
-    
+
 }
